2af116194b398a0bed60ee2b699b8fb8e0ed6f48
Eugene <ezhikn@gmail.com>
2018-05-10 23:12:29 +0300

    Задание 4 с маленькой правкой

--- src/main/java/util/AllureReporter.java
+++ src/main/java/util/AllureReporter.java
@@ -1,13 +1,28 @@
 package util;
 
-import org.junit.runner.notification.Failure;
-import ru.yandex.qatools.allure.junit.AllureRunListener;
-import steps.BaseSteps;
+import gherkin.formatter.model.Result;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import ru.yandex.qatools.allure.Allure;
+import ru.yandex.qatools.allure.events.MakeAttachmentEvent;
 
-public class AllureReporter extends AllureRunListener {
+
+import static steps.BaseSteps.getDriver;
+
+public class AllureReporter extends ru.yandex.qatools.allure.cucumberjvm.AllureReporter{
+
     @Override
-    public void testFailure(Failure failure){
-        BaseSteps.takeScreenshot();
-        super.testFailure(failure);
+    public void result(Result result) {
+        if ("failed".equals(result.getStatus())) takeScreenshot(result);
+        super.result(result);
+    }
+
+
+
+    public void takeScreenshot(Result step) {
+        if (getDriver() != null) {
+            Allure.LIFECYCLE.fire(new MakeAttachmentEvent(((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.BYTES),
+                    "Скриншот в момент ошибки", "image/png"));
+        }
     }
 }
